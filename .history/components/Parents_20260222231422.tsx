cdc619b633a4375f83c8d9fc3ee771dc71003d8b
Cruz Prieto Jose Luis <141293124+Joseluiscruz-hub@users.noreply.github.com>
2026-02-23 08:38:02 -0600

    Actualizar Parents_20260222231422.tsx

--- .history/components/Parents_20260222231422.tsx
+++ .history/components/Parents_20260222231422.tsx
@@ -68,7 +68,7 @@ export const Parents: React.FC = () => {
           <div className="grid grid-cols-1 md:grid-cols-2 gap-12 mb-16">
             <ParentCard
               title="Mi Papá"
-              names={['José Luis Cruz']}
+              names={['José Luis Cruz Prieto']}
               delay={0.2}
               icon={<Crown size={24} />}
             />
@@ -115,19 +115,19 @@ export const Parents: React.FC = () => {
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
             <ParentCard
               title="Padrinos de Velación"
-              names={['JOSE ARTURO GONZALEZ CHAVEZ', 'ELSA GONZALEZ LOPEZ']}
+              names={['Jose Arturo Gonzales Chaves', 'Elsa Gonzales Lopez']}
               delay={0.5}
               icon={<Heart size={20} />}
             />
             <ParentCard
               title="Padrinos de Anillo"
-              names={['JOSE ARTURO GONZALEZ CHAVEZ', 'ELSA GONZALEZ LOPEZ']}
+              names={['Jose Arturo Gonzales Chaves', 'Elsa Gonzales Lopez']}
               delay={0.6}
               icon={<Heart size={20} />}
             />
             <ParentCard
               title="Padrinos de Corona"
-              names={['JOSE ARTURO GONZALEZ CHAVEZ', 'ELSA GONZALEZ LOPEZ']}
+              names={['Jose Arturo Gonzales Chaves', 'Elsa Gonzales Lopez']}
               delay={0.7}
               icon={<Heart size={20} />}
             />
